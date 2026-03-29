988e66a9eaa340cfb92f9a9e2a8bf7d09670dca1
Diego Rigato <diego.rigato@hotmail.com>
2026-03-29 15:39:19 -0300

    POC Atividade inicial

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sistema Clothes4Happiness rodando com CI/CD e Docker 🚀"
+
+@app.route("/status")
+def status():
+    return {"status": "online", "sistema": "Clothes4Happiness"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
